4d360e77cff843daf0785e279a863401d6eac62c
Kelvin Anigboro <rukywe@me.com>
2024-05-16 09:31:36 +0100

    feat: Add middleware for logging in router
    This commit adds a new middleware function for logging in the router. The middleware function is added to the router in the function. This will enable logging of incoming requests and their corresponding responses.

--- routers/router.go
+++ routers/router.go
@@ -3,12 +3,15 @@ package routers
 import (
 	"github.com/gorilla/mux"
 	"github.com/rukywe/go-books-api/handlers"
+	"github.com/rukywe/go-books-api/middleware"
 	"github.com/rukywe/go-books-api/storage"
 )
 
 func NewRouter(store *storage.InMemoryStore) *mux.Router {
     router := mux.NewRouter()
     bookHandler := handlers.BookHandler{Store: store}
+
+    router.Use(middleware.Logger)
 
     router.HandleFunc("/books", bookHandler.CreateBook).Methods("POST")
     router.HandleFunc("/books/{id}", bookHandler.GetBook).Methods("GET")
